4c6d56c55085c1640a9790e64530f83df8a35b2f
thapelo <tmmakola021@student.wethinkcode.co.za>
2023-10-25 01:55:07 +0200

    subscribe page

---
+++ src/pages/subscribe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from "styled-components";
+import "../css/style.css";
+
+const Subscribe = () => {
+    return (
+        <div
+        style={{
+            fontWeight:'normal'
+        }}>
+
+        <Text>
+            <text>
+                <h1>Needle Drops & Reviews.</h1>
+                <h1>Subscribe to Vaultgeist!</h1>
+                <p>Subscribe to our newsletter, and Elevate Your Vinyl Experience</p>
+                <StyledForm>
+                    <form>
+                    <input type="text" name="user_email "placeholder='Whats your Email?'/>
+                    <input type="submit" value="Send" />
+                    </form>
+                </StyledForm>
+                
+
+
+            </text>
+        </Text>  
+        </div>
+    );
+};
+export default Subscribe;
